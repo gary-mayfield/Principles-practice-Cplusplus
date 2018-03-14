7c60d0f9cf76ea68b05f2857bb6d4fbfee7ca708
gary-mayfield <gary.mayfield@hotmail.co.uk>
2018-03-14 20:02:37 +0000

    Example 6-3-3 added

---
+++ Chapter06/chapter-6-3-3.cpp
@@ -0,0 +1,32 @@
+
+//
+// This is an example from Chapter ..." of
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+// - Gary Mayfield
+
+#include "std_lib_facilities.h"
+
+//------------------------------------------------------------------------------
+
+class Token{            // a very simple user-defined type
+public:
+    char kind;
+    double value;
+};
+
+int main()
+{
+    Token t;            // t is a Token
+    t.kind = '+';       // t represents a +
+    Token t2;           // t2 is another Token
+    t2.kind = '8';      // we use the digit 8 as the 'kind' for numbers
+    t2.value = 3.14;
+    Token tt = t;       // copy initialization
+    if(tt.kind != t.kind) error("impossible!");
+    t = t2;             // assignment
+    cout << t.value;    // will print 3.14
+    keep_window_open();
+    return 0;
+}
+
+//------------------------------------------------------------------------------
