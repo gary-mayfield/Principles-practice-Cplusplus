81cca49a6f953b8858dace7c631d414e3cd0e2c9
gary-mayfield <gary.mayfield@hotmail.co.uk>
2018-04-02 13:48:30 +0100

    Chapter 9 started

---
+++ Chapter09/framework.cpp
@@ -0,0 +1,17 @@
+
+//
+// This is an example from Chapter ..." of
+// "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
+// - Gary Mayfield
+
+#include "std_lib_facilities.h"
+
+//------------------------------------------------------------------------------
+
+int main()
+{
+    keep_window_open();
+    return 0;
+}
+
+//------------------------------------------------------------------------------
